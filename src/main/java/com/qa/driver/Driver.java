252be60d151ac5ef7d06296fdd1f7aba4d7ebd4a
Bharath <bharathnotakar@gmail.com>
2024-05-16 08:21:09 +0530

    added the explicitly wait method in basepage

--- src/main/java/com/qa/driver/Driver.java
+++ src/main/java/com/qa/driver/Driver.java
@@ -1,11 +1,9 @@
 package com.qa.driver;
 
-import java.time.Duration;
 import java.util.Objects;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.qa.constants.FrameworkConstants;
 import com.qa.utilities.ReadPropertiesFile;
 
 public class Driver {
@@ -19,8 +17,6 @@ public class Driver {
 			DriverManager.setDriver(new ChromeDriver());
 			DriverManager.getDriver().get(ReadPropertiesFile.getPropertyValueByHashMap("url"));
 			DriverManager.getDriver().manage().window().maximize();
-			DriverManager.getDriver().manage().timeouts()
-					.implicitlyWait(Duration.ofSeconds(FrameworkConstants.getWaitTime()));
 		}
 	}
 
